924b59fc37afe046c22c811b105caf2824aa5afd
锦添 <jintian.deng@gmail.com>
2013-01-10 23:30:13 +0800

    Add upload progress report. Some code optimizations.

--- src/main/java/com/dengjintian/uploader/Main.java
+++ src/main/java/com/dengjintian/uploader/Main.java
@@ -1,58 +1,64 @@
 package com.dengjintian.uploader;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.io.IOException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
 
 /**
  * This program is a simple application to upload file to dropbox. <br/>
  * It can be used with cron to backup files.
  * <p>
  * It accept one system property "timeout" which means how long the process <br />
- * will wait for the finish of uploading.    Default value is 1 minute.
+ * will wait for the finish of uploading. Default value is 1 minute.
  * </p>
  * <p>
- * Sample usage: java -Dtimeout=5 -jar dropbox.jar
+ * Sample usage: java -Dtimeout=5 -jar dropbox.jar  xxx.text
  * </p>
  */
 public class Main {
 
-    static final String APP_KEY = "";
-    static final String APP_SECRET = "";
-    static final String ACCESS_KEY = "";
-    static final String ACCESS_SECRET = "";
-    private static final Log log = LogFactory.getLog(Main.class);
+    private static final Log log           = LogFactory.getLog(Main.class);
+    public static String     APP_KEY       = "";
+    public static String     APP_SECRET    = "";
+    public static String     ACCESS_KEY    = "";
+    public static String     ACCESS_SECRET = "";
 
     public static void main(String[] args) throws IOException {
+        // return if no files is specified
         if (args == null || args.length == 0) {
-            log.info("File list is empty!");
+            log.error("File list is empty!");
             return;
         }
-        log.info("Start uploading file...");
 
+        // start uploading
+        log.info("Start uploading file...");
         FutureTask<Boolean> futureTask = new FutureTask<Boolean>(new Uploader(args));
         new Thread(futureTask).start();
 
-        //get the timeout
+        // get the timeout and wait for finnish
         String timeoutString = System.getProperty("timeout");
         Long minutes = 1l;
         if (StringUtils.isNotBlank(timeoutString) && StringUtils.isNumeric(timeoutString)) {
             minutes = Long.valueOf(timeoutString);
         }
-        log.warn("Max wait time " + minutes + " minutes!");
-
+        log.warn("Max wait " + minutes + " minutes!");
         if (!futureTask.isDone()) {
             try {
                 futureTask.get(minutes, TimeUnit.MINUTES);
-            } catch (Exception e) {
-                log.error("Something wrong happen!", e);
+            } catch (InterruptedException e) {
+                log.error("Uploading is interrupted!", e);
+            } catch (ExecutionException e) {
+                log.error("Something wrong during uploading!", e);
+            } catch (TimeoutException e) {
+                log.error("Uploading timeout!", e);
             }
         }
-
         log.info("Finishing uploading file...");
     }
 }
--- src/main/java/com/dengjintian/uploader/Uploader.java
+++ src/main/java/com/dengjintian/uploader/Uploader.java
@@ -1,18 +1,21 @@
 package com.dengjintian.uploader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.dropbox.client2.DropboxAPI;
 import com.dropbox.client2.DropboxAPI.Entry;
+import com.dropbox.client2.ProgressListener;
 import com.dropbox.client2.exception.DropboxException;
 import com.dropbox.client2.session.AccessTokenPair;
 import com.dropbox.client2.session.AppKeyPair;
 import com.dropbox.client2.session.Session.AccessType;
 import com.dropbox.client2.session.WebAuthSession;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.concurrent.Callable;
 
 /**
  * Upload a list of files to dropbox server.
@@ -20,9 +23,9 @@ import java.util.concurrent.Callable;
 public class Uploader implements Callable<Boolean> {
 
     private static final Log log = LogFactory.getLog(Uploader.class);
-    private String[] fileList;
+    private String[]         fileList;
 
-    public Uploader(String[] fileList) {
+    public Uploader(String[] fileList){
         this.fileList = fileList;
     }
 
@@ -32,45 +35,77 @@ public class Uploader implements Callable<Boolean> {
             return false;
         }
 
-        //create dropbox api
+        // create dropbox api
+        DropboxAPI<WebAuthSession> api = getDropboxApi();
+
+        // upload each file
+        long beginning = System.currentTimeMillis();
+        for (String file : fileList) {
+            uploadFile(file, api);
+        }
+        beginning = System.currentTimeMillis() - beginning;
+        log.info("Upload finished in " + beginning + "ms !");
+        return true;
+    }
+
+    private DropboxAPI<WebAuthSession> getDropboxApi() {
         AccessTokenPair accessTokenPair = new AccessTokenPair(Main.ACCESS_KEY, Main.ACCESS_SECRET);
         WebAuthSession session = new WebAuthSession(new AppKeyPair(Main.APP_KEY, Main.APP_SECRET), AccessType.DROPBOX,
-                accessTokenPair);
-
+                                                    accessTokenPair);
         DropboxAPI<WebAuthSession> api = new DropboxAPI<WebAuthSession>(session);
+        return api;
+    }
 
-        //upload each file
-        long timeCost = System.currentTimeMillis();
-        for (String file : fileList) {
-            File f = new File(file);
-            if (!f.exists()) {
-                log.error("File(" + file + ") does not exist!");
-                continue;
-            }
-
-            log.info("Uploading file " + file + " with size " + f.length() / 1024 + "KB");
-            String dropboxFileName = file.startsWith("/") ? file : "/" + file;
-
-            //try to get the old version of the file if exist
-            Entry meta = null;
-            try {
-                meta = api.metadata(dropboxFileName, 1, null, false, null);
-                if (meta != null) log.info("Old rev " + meta.rev);
-            } catch (DropboxException e) {
-                log.warn("File(" + file + ") may not exist in dropbox server!", e);
-            }
-
-            try {
-                api.putFile(dropboxFileName, new FileInputStream(file), f.length(), meta == null ? null : meta.rev, null);
-            } catch (Exception e) {
-                log.error("Upload file(" + file + ") fail!", e);
-                continue;
-            }
-            log.info("Upload file(" + file + ") success!");
+    private void uploadFile(String file, DropboxAPI<WebAuthSession> api) {
+        File f = new File(file);
+        if (!f.exists()) {
+            log.error("File(" + file + ") does not exist!");
+            return;
         }
-        timeCost = System.currentTimeMillis() - timeCost;
-        log.info("Upload finished in " + timeCost + "ms !");
-        return true;
+
+        log.info("Uploading file " + file + " with size " + f.length() / 1024 + "KB");
+        String dropboxFileName = file.startsWith("/") ? file : "/" + file;
+
+        // try to get the old version of the file if exist
+        Entry meta = null;
+        try {
+            meta = api.metadata(dropboxFileName, 1, null, false, null);
+            if (meta != null) log.info("Old rev " + meta.rev);
+        } catch (DropboxException e) {
+            log.warn("File(" + file + ") may not exist in dropbox server!", e);
+        }
+
+        // do uploading
+        try {
+            meta = api.putFile(dropboxFileName, new FileInputStream(file), f.length(), meta == null ? null : meta.rev,
+                               getProgressListener());
+        } catch (Exception e) {
+            log.error("Upload file(" + file + ") fail!", e);
+            return;
+        }
+        log.info("Upload file(" + file + ") success! New rev is " + meta.rev);
+    }
+
+    /**
+     * print the uploading progress.
+     * 
+     * @return
+     */
+    public ProgressListener getProgressListener() {
+        ProgressListener progressListener = new ProgressListener() {
+            @Override
+            public void onProgress(long bytes, long total) {
+                String anim = "|/-\\";
+                int x = (int) (bytes * 1.0 / total * 100);
+                String data = "\r" + anim.charAt(x % anim.length()) + " " + x + "%\r";
+                try {
+                    System.out.write(data.getBytes());
+                } catch (IOException e) {
+                    // do nothing, be quite if something wrong happens
+                }
+            }
+        };
+        return progressListener;
     }
 
     @Override
